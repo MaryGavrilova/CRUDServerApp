69c1f5923775c0f58c8befa180634a31d9d8f4da
Mariia Gavrilova <mariya.zuzina@mera.com>
2021-10-11 18:55:15 +0300

    First version

---
+++ src/main/java/ru/netology/handlers/PostHandler.java
@@ -0,0 +1,12 @@
+package ru.netology.handlers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@FunctionalInterface
+public interface PostHandler {
+
+void handle(HttpServletRequest req, HttpServletResponse resp) throws IOException;
+
+}
---
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,101 @@
+package ru.netology.servlet;
+
+import ru.netology.controller.PostController;
+
+import ru.netology.handlers.PostHandler;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainServlet extends HttpServlet {
+    private static final String GET = "GET";
+    private static final String PATH_FOR_ALL_POSTS = "/api/posts";
+    private static final String PATH_FOR_POST_WITH_ID = "/api/posts/\\d+";
+    private static final String POST = "POST";
+    private static final String DELETE = "DELETE";
+    private PostController controller;
+    private Map<String, Map<String, PostHandler>> postHandlers;
+
+    @Override
+    public void init() {
+        final PostRepository repository = new PostRepository();
+        final PostService service = new PostService(repository);
+        controller = new PostController(service);
+        postHandlers = new HashMap<>();
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            final String path = req.getRequestURI();
+            final String method = req.getMethod();
+
+            addHandler(GET, PATH_FOR_ALL_POSTS, new PostHandler() {
+                @Override
+                public void handle(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+                    controller.all(resp);
+                }
+            });
+            addHandler(GET, PATH_FOR_POST_WITH_ID, new PostHandler() {
+                @Override
+                public void handle(HttpServletRequest req, HttpServletResponse resp) {
+                    final long id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+                    controller.getById(id, resp);
+                }
+            });
+            addHandler(POST, PATH_FOR_ALL_POSTS, new PostHandler() {
+                @Override
+                public void handle(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+                    controller.save(req.getReader(), resp);
+                }
+            });
+            addHandler(DELETE, PATH_FOR_POST_WITH_ID, new PostHandler() {
+                @Override
+                public void handle(HttpServletRequest req, HttpServletResponse resp) {
+                    final long id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+                    controller.removeById(id, resp);
+                }
+            });
+
+            for (Map.Entry<String, Map<String, PostHandler>> methodVariants : postHandlers.entrySet()) {
+                if (methodVariants.getKey().equals(method)) {
+                    Map<String, PostHandler> handlersForMethod = methodVariants.getValue();
+                    for (Map.Entry<String, PostHandler> pathVariants : handlersForMethod.entrySet()) {
+                        if (pathVariants.getKey().matches(path)) {
+                            PostHandler postHandler = pathVariants.getValue();
+                            postHandler.handle(req, resp);
+                            return;
+                        }
+                    }
+                }
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public void addHandler(String method, String path, PostHandler postHandler) {
+        if (postHandlers.containsKey(method)) {
+            for (Map.Entry<String, Map<String, PostHandler>> methodVariants : postHandlers.entrySet()) {
+                if (methodVariants.getKey().equals(method)) {
+                    Map<String, PostHandler> handlersForMethod = methodVariants.getValue();
+                    handlersForMethod.put(path, postHandler);
+                }
+            }
+        } else {
+            postHandlers.put(method, new HashMap<String, PostHandler>() {{
+                put(path, postHandler);
+            }});
+        }
+    }
+}
+
+
